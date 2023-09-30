f72db8ff61935acf6eaa2d1a1fe60391ffbff697
mielnikmagdalena <mielnikmagdalena@gmail.com>
2023-09-30 15:53:40 +0200

    the newest version of RH6

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 // src/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './redux/contactsReducer';
-
+import filterReducer from './redux/filterReducer';
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer, // Reduktor kontaktów
